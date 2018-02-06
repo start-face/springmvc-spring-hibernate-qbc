4500e4d522ab42a0a74b6ed53b7b4f1bc6208619
FaceFeel <tancairong0624>
2018-02-06 23:42:03 +0800

    1.临时提交

--- src/main/java/com/ssh/dao/impl/UserDaoImpl.java
+++ src/main/java/com/ssh/dao/impl/UserDaoImpl.java
@@ -1,11 +1,17 @@
 package com.ssh.dao.impl;
+
 import com.ssh.dao.UserDao;
 import com.ssh.model.UserModel;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @author FaceFeel
@@ -17,6 +23,28 @@ public class UserDaoImpl implements UserDao {
     @Resource
     private HibernateTemplate hibernateTemplate;
 
+
+    /**
+     * QBC方式查询
+     */
+    public void test() {
+
+        SessionFactory sessionFactory = hibernateTemplate.getSessionFactory();
+        Session currentSession = sessionFactory.getCurrentSession();
+        Criteria criteria = currentSession.createCriteria(UserModel.class);
+        UserModel userModel = new UserModel();
+        userModel.setAge(23).setUserName("john");
+
+        Example example = Example.create(userModel);
+        criteria.add(example);
+        List list = criteria.list();
+
+        list.forEach(one -> {
+            UserModel user = (UserModel) one;
+            System.err.println(user);
+        });
+    }
+
     @Override
     public Boolean addUser(UserModel userModel) {
 
@@ -25,7 +53,7 @@ public class UserDaoImpl implements UserDao {
             Serializable save = hibernateTemplate.save(userModel);
             System.err.println(save);
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
